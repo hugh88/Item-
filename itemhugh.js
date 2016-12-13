20bebb82a073ed245145c64c7746b6ce040aa9a9
hugh88 <7hgallagly@stpatrick.org>
2016-12-13 10:50:15 -0600

    Item
    accidentally messed up yesterday submitting this so i had to redo it

---
+++ itemhugh.js
@@ -0,0 +1,12 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor (name,price,shipping){
+    this.name= n;
+    this.price= p;
+    this.shipping= s;
+}
+
+//Create your three test items below this comment.
+let Harddrive = new item("Hybrid",80,2)
+let Ammo = new item("30-06",20,3)
+let gloves = new item("Machinex",40,1)
